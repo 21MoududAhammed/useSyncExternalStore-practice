645b37d58bc05de988de419f2ba8aa04318bb46a
21MoududAhammed <21moudud@gmail.com>
2024-02-16 09:14:19 +0600

    done

--- src/hooks/useOnlineStatus.js
+++ src/hooks/useOnlineStatus.js
@@ -1,24 +1,23 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useSyncExternalStore } from "react";
+
+function subscribe(callback) {
+  // synchronizing
+  window.addEventListener("online", callback);
+  window.addEventListener("offline", callback);
+
+  //clean up
+  return () => {
+    window.removeEventListener("online", callback);
+    window.removeEventListener("offline", callback);
+  };
+}
 
 export default function useOnlineStatus() {
-  const [isOnline, setIsOnline] = useState(true);
-
-  useEffect(() => {
-    function updateState() {
-      setIsOnline(navigator.onLine); // navigator.onLine is a browser api which let you know the device is online or offline
-    }
-    updateState();
-    // synchronizing
-    window.addEventListener("online", updateState);
-    window.addEventListener("offline", updateState);
-
-    //clean up
-    return () => {
-      window.removeEventListener("online", updateState);
-      window.removeEventListener("offline", updateState);
-    };
-  }, []);
+  const isOnline = useSyncExternalStore(
+    subscribe,
+    () => navigator.onLine,
+    () => true
+  );
 
   return isOnline;
 }
